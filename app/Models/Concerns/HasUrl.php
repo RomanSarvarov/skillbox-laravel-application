a82c81aafc3a71bd1b2c59a5521f4a4cd4dcabc9
Roman Sarvarov <roman.sarvarov@gmail.com>
2020-05-11 00:37:16 +0300

    has url update

--- app/Models/Concerns/HasUrl.php
+++ app/Models/Concerns/HasUrl.php
@@ -6,6 +6,10 @@ trait HasUrl
 {
     public function getUrlAttribute(): string
     {
+        if ($this->id === null) {
+            return '';
+        }
+
         $routeName = $this->getRouteName();
 
         return route("$routeName.show", $this);
